820d32bbf09732f559091f8c9bf1a3f82590af99
Serin-Kim <serin9864@gmail.com>
2022-04-08 22:36:42 +0900

    CameraActivity 추가

---
+++ app/src/main/java/com/example/starbucksook/CameraActivity.java
@@ -0,0 +1,127 @@
+package com.example.starbucksook;
+
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import androidx.core.content.FileProvider;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareDialog;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+import static android.os.Environment.DIRECTORY_PICTURES;
+
+public class CameraActivity extends AppCompatActivity {
+
+    public static final int REQUEST_IMAGE_CAPTURE = 672;
+    public String imageFilePath;
+
+    CallbackManager callbackManager;
+    ShareDialog shareDialog;
+    Button btnFacebook;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        // 받아온 이미지 경로
+        Intent intent2 = getIntent();
+        imageFilePath = intent2.getStringExtra("imageFilePath");
+
+        // 비트맵 변환
+        Bitmap bitmap = BitmapFactory.decodeFile(imageFilePath);
+        ExifInterface exif = null;
+
+        try {
+            exif = new ExifInterface(imageFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int exifOrientation;
+        int exifDegree;
+
+        if (exif != null) {
+            exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            exifDegree = exifOrientationToDegress(exifOrientation);
+        } else {
+            exifDegree = 0;
+//            exifDegree = 270;
+
+        }
+
+        // 이미지 띄우기
+        ((ImageView) findViewById(R.id.iv_result)).setImageBitmap(rotate(bitmap, exifDegree));
+
+        FacebookSdk.sdkInitialize(getApplicationContext()); // 페이스북SDK 초기화
+        callbackManager = CallbackManager.Factory.create(); // 콜백메소드 생성
+        shareDialog = new ShareDialog(this); // 공유를 위한 다이얼로그 박스
+
+        btnFacebook = (Button)findViewById(R.id.btnFacebook);
+        btnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ShareDialog.canShow(ShareLinkContent.class)) {
+                    Bitmap image = BitmapFactory.decodeFile(imageFilePath);
+                    SharePhoto photo = new SharePhoto.Builder()
+                            .setBitmap(rotate(image, exifDegree)) // 페이스북에도 회전 적용
+                            .build();
+                    SharePhotoContent content = new SharePhotoContent.Builder()
+                            .addPhoto(photo)
+                            .build();
+
+                    shareDialog.show(content);
+                }
+            }
+        });
+    }
+
+    // 카메라에 맞게 이미지 로테이션
+    public Bitmap rotate(Bitmap bitmap, float degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(),
+                matrix, true);
+    }
+
+    public int exifOrientationToDegress(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+//        return 270;
+    }
+}
